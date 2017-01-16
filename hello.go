d3afc290b815f837b6e0ffdc443c7c908abe3384
Hao Ho <hh186013@teradata.com>
2017-01-16 13:31:32 -0800

    added range.go

--- hello.go
+++ hello.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+
+    "github.com/hqho/stringutil"
+)
 
 func main() {
-        fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+    fmt.Printf("\n")
+
+/*
+    b := [...]string{"Penn", "Teller"}
+    fmt.Println("b:", b)
+*/
+
+    letters := []string{"a", "b", "c", "d"}
+    fmt.Println("letters:", letters)
+
+    var s []byte
+    s = make([]byte, 5, 5)
+    t := make([]byte, len(s), (cap(s)+1)*2) // +1 in case cap(s) == 0
+    copy(t, s)
+    s = t
+    fmt.Println("s:", s)
+
+    a := []string{"John", "Paul"}
+    b := []string{"George", "Ringo", "Pete"}
+    a = append(a, b...) // equivalent to "append(a, b[0], b[1], b[2])"
+    fmt.Println("a:", a)
 }
